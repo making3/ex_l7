83b9f0793635dadf9adc12cad7dcd2900ed5bb82
Matthew King <mattking73@gmail.com>
2018-02-09 13:02:57 -0800

    Add message struct and to_string

--- lib/ex_l7/message.ex
+++ lib/ex_l7/message.ex
@@ -1,4 +1,24 @@
 defmodule ExL7.Message do
-  # TODO: Define Struct
-  defstruct []
+  @moduledoc """
+  Documentation for ExL7.Message
+  """
+  alias ExL7.QueryParser
+  alias ExL7.Segment
+
+  defstruct segments: [], timezone: "UTC", control_characters: %ExL7.ControlCharacters{}
+
+  def query(query_string, date_time_format) do
+    # TODO: Implementation
+    {:ok, query} = QueryParser.parse(query_string)
+  end
+
+  def to_string(message, control_characters) do
+    hl7 =
+      message.segments
+      |> Enum.map(&Segment.to_string(&1, control_characters))
+      |> Enum.join(control_characters.segment)
+
+    # Messages always end with a segment control character, usually a line feed ending
+    hl7 <> control_characters.segment
+  end
 end
