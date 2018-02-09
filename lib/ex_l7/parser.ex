7a612af45e7ec74a031f18dd8de5af80975a38ed
Matthew King <mattking73@gmail.com>
2018-02-09 09:55:03 -0800

    Add segment delimiter parameter to parsing

--- lib/ex_l7/parser.ex
+++ lib/ex_l7/parser.ex
@@ -5,28 +5,35 @@ defmodule ExL7.Parser do
 
   import ExL7.Validation
 
-  def parse(hl7_string) do
+  def parse(hl7_string, segment_delimiter \\ "\r") do
     validate(hl7_string)
-
-    # TODO: Implementation
+    |> do_parse(hl7_string, segment_delimiter)
   end
 
-  def parse_file(file_with_hl7) do
+  defp do_parse(err_result = {:error, _}, _hl7, _segment_delimiter) do
+    err_result
+  end
+
+  defp do_parse({:ok, _}, hl7, segment_delimiter) do
+    String.split(hl7, segment_delimiter)
+  end
+
+  def parse_file(file_with_hl7, segment_delimiter \\ "\r") do
     file_with_hl7
     |> File.exists?()
-    |> read_file(file_with_hl7)
+    |> read_file(file_with_hl7, segment_delimiter)
   end
 
-  defp read_file(false, _file_name) do
+  defp read_file(false, _file_name, _segment_delimiter) do
     {:error, "Invalid File"}
   end
 
-  defp read_file(true, file_name) do
+  defp read_file(true, file_name, segment_delimiter) do
     {:ok, file} = File.open(file_name, [:read])
 
     case IO.read(file, :all) do
       :eof -> {:error, "No Data"}
-      hl7 -> parse(hl7)
+      hl7 -> parse(hl7, segment_delimiter)
     end
   end
 end
