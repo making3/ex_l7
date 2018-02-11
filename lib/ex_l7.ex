e50800d361a34fe413e9c2e0a500907e1c71e2bd
Matthew King <mattking73@gmail.com>
2018-02-11 15:01:37 -0800

    Add ExDoc/Earmark, Update some doc examples, create parse/validate functions in lib/ex_l7.ex that just passes to their actual modules, remove timezone tests

--- lib/ex_l7.ex
+++ lib/ex_l7.ex
@@ -1,18 +1,51 @@
 defmodule ExL7 do
   @moduledoc """
-  Documentation for ExL7.
+  HL7 parsing and querying application
   """
 
-  @doc """
-  Hello world.
+  @doc ~S"""
+  Parses an HL7 message into an ExL7.Message for ExL7 usage. Validates first.
+    For validation examples, check ExL7.Validation.validate.
+
+    ## Parameters
+
+    - hl7: HL7 message to parse.
+    - segment_delimiter: An alternative value other than \\r to split message segments.
+  """
+  def parse(hl7, segment_delimiter \\ "\r") do
+    ExL7.Parser.parse(hl7, segment_delimiter)
+  end
+
+  @doc ~S"""
+  Checks if an HL7 message is valid.
+
+  ## Parameters
+
+  - hl7: HL7 message to validate.
+  - segment_delimiter: An alternative value other than \\r to split message segments.
 
   ## Examples
 
-      iex> ExL7.hello
-      :world
+      iex> ExL7.validate("")
+      {:error, "No Data"}
+
+      iex> ExL7.validate("MSH|")
+      {:error, "Invalid Header"}
+
+      iex> ExL7.validate("MSH|")
+      {:error, "Invalid Header"}
+
+      iex> ExL7.validate("MSH|^~\\&|ExL7|iWT Health||1|||ORU^R01||T|2.4")
+      {:error, "No Segments Found"}
+
+      iex> ExL7.validate("MSH|^~\\&|ExL7|iWT Health||1|||ORU^R01||T|2.4\rPI\rPIDD")
+      {:error, "Invalid Segment(s)"}
+
+      iex> ExL7.validate("MSH|^~\\&|ExL7|iWT Health||1|||ORU^R01||T|2.4\rPID\rXFA")
+      {:ok, nil}
 
   """
-  def hello do
-    :world
+  def validate(hl7, segment_delimiter \\ "\r") do
+    ExL7.Validation.validate(hl7, segment_delimiter)
   end
 end
--- lib/ex_l7/control_characters.ex
+++ lib/ex_l7/control_characters.ex
@@ -1,6 +1,6 @@
 defmodule ExL7.ControlCharacters do
   @moduledoc """
-  Documentation for ExL7.Parser
+  Parses control characters for an HL7 message from the header.
   """
   defstruct segment: "\r",
             field: "|",
@@ -9,6 +9,18 @@ defmodule ExL7.ControlCharacters do
             escape: "\\",
             sub_component: "&"
 
+  @doc ~S"""
+  Returns the control characters for an HL7 message
+
+  ## Parameters
+
+  - hl7: The HL7 message or MSH header
+  - segment_delimiter: Character that separates HL7 segments, defaults to \\r
+
+  ## Examples
+    iex>ExL7.ControlCharacters.get_control_characters("MSH|^~\\&")
+    %ExL7.ControlCharacters{segment: "\r", field: "|", component: "^", repeat: "~", escape: "\\", sub_component: "&"}
+  """
   def get_control_characters(hl7, segment_delimiter \\ "\r") do
     control_character_regex =
       ~r/^MSH(?<field>.)(?<component>.)(?<repeat>.)(?<escape>.)(?<sub_component>.)/i
