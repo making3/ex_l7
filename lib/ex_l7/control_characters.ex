2ec4f9066374ba59a2848e9b8ce2af465ba94080
Matthew King <mattking73@gmail.com>
2018-02-26 10:41:44 -0800

    Add clarity to convert_to_struct

--- lib/ex_l7/control_characters.ex
+++ lib/ex_l7/control_characters.ex
@@ -31,14 +31,14 @@ defmodule ExL7.ControlCharacters do
     |> convert_to_struct()
   end
 
-  defp convert_to_struct(control_characters) do
+  defp convert_to_struct(%{} = control_characters_map) do
     %ExL7.ControlCharacters{
-      segment: control_characters["segment"],
-      field: control_characters["field"],
-      component: control_characters["component"],
-      repeat: control_characters["repeat"],
-      escape: control_characters["escape"],
-      sub_component: control_characters["sub_component"]
+      segment: control_characters_map["segment"],
+      field: control_characters_map["field"],
+      component: control_characters_map["component"],
+      repeat: control_characters_map["repeat"],
+      escape: control_characters_map["escape"],
+      sub_component: control_characters_map["sub_component"]
     }
   end
 end
